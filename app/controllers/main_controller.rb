0c3d29284f5ad24404fb619a7700345b7c6391e6
ankit-prajapati-26 <ankit.p@browserstack.com>
2021-10-14 14:49:22 +0530

    flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,6 @@
 class MainController < ApplicationController
     def index
+        flash[:info] = 'Logged in successful' #flash without .now caches the it for one more request
+        flash.now[:alert] = 'Something went wrong'
     end
 end
