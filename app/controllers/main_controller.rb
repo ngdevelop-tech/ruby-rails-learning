c9e2c09559c51df20aaf46b18f621770e336a796
ankit-prajapati-26 <ankit.p@browserstack.com>
2021-10-18 11:24:56 +0530

    Storing session in Current Attributes to access it in multiple places

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,5 @@ class MainController < ApplicationController
   def index
     # flash[:success] = 'Logged in successful' #flash without .now caches the it for one more request
     # flash.now[:error] = 'Something went wrong'
-
-    @user = User.find_by(id: session[:user_id])
   end
 end
